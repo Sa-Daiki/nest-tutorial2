ffc58a5f3f79b2806db68f902d3a9cdc55195606
Sa-Daiki <sato-daiki@evolu.tokyo>
2023-02-21 20:14:59 +0900

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ItemsModule } from './items/items.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [ItemsModule, AuthModule, TypeOrmModule.forRoot()],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
